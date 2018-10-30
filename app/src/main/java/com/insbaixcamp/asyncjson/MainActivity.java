b2c38488fbb06fe58a55c6285c277af83a312169
jaume5900 <deepblarn@gmail.com>
2018-10-30 15:28:16 +0100

    Async task

--- app/src/main/java/com/insbaixcamp/asyncjson/MainActivity.java
+++ app/src/main/java/com/insbaixcamp/asyncjson/MainActivity.java
@@ -17,7 +17,7 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
 
     public ArrayList<DataModel> dataModels;
@@ -33,54 +33,11 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
 
-
-        setSupportActionBar(toolbar);
-
         listView=(ListView)findViewById(R.id.lvAndroid);
 
-        dataModels= new ArrayList<>();
-
-
-
-        dataModels.add(new DataModel("Apple Pie", "Android 1.0", "1","September 23, 2008"));
-        dataModels.add(new DataModel("Banana Bread", "Android 1.1", "2","February 9, 2009"));
-        dataModels.add(new DataModel("Cupcake", "Android 1.5", "3","April 27, 2009"));
-        dataModels.add(new DataModel("Donut","Android 1.6","4","September 15, 2009"));
-        dataModels.add(new DataModel("Eclair", "Android 2.0", "5","October 26, 2009"));
-        dataModels.add(new DataModel("Froyo", "Android 2.2", "8","May 20, 2010"));
-        dataModels.add(new DataModel("Gingerbread", "Android 2.3", "9","December 6, 2010"));
-        dataModels.add(new DataModel("Honeycomb","Android 3.0","11","February 22, 2011"));
-        dataModels.add(new DataModel("Ice Cream Sandwich", "Android 4.0", "14","October 18, 2011"));
-        dataModels.add(new DataModel("Jelly Bean", "Android 4.2", "16","July 9, 2012"));
-        dataModels.add(new DataModel("Kitkat", "Android 4.4", "19","October 31, 2013"));
-        dataModels.add(new DataModel("Lollipop","Android 5.0","21","November 12, 2014"));
-        dataModels.add(new DataModel("Marshmallow", "Android 6.0", "23","October 5, 2015"));
-
-
-        adapter= new CustomAdapter(dataModels,getApplicationContext());
-
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                DataModel dataModel = dataModels.get(position);
-
-                Snackbar.make(view, dataModel.getName()+"\n"+dataModel.getType()+" API: "+dataModel.getVersion_number(), Snackbar.LENGTH_LONG)
-                        .setAction("No action", null).show();
-            }
-        });
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Testing", Toast.LENGTH_LONG).show();
-            }
-        });
+        fab.setOnClickListener(this);
     }
-
-
 
 
     @Override
@@ -103,5 +60,64 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        new getJSON().execute();
+
+    }
+
+
+    private class getJSON extends AsyncTask<String, Void, String>{
+
+
+
+        @Override
+        protected String doInBackground(String... strings) {
+            dataModels.add(new DataModel("Apple Pie", "Android 1.0", "1","September 23, 2008"));
+            dataModels.add(new DataModel("Banana Bread", "Android 1.1", "2","February 9, 2009"));
+            dataModels.add(new DataModel("Cupcake", "Android 1.5", "3","April 27, 2009"));
+            dataModels.add(new DataModel("Donut","Android 1.6","4","September 15, 2009"));
+            dataModels.add(new DataModel("Eclair", "Android 2.0", "5","October 26, 2009"));
+            dataModels.add(new DataModel("Froyo", "Android 2.2", "8","May 20, 2010"));
+            dataModels.add(new DataModel("Gingerbread", "Android 2.3", "9","December 6, 2010"));
+            dataModels.add(new DataModel("Honeycomb","Android 3.0","11","February 22, 2011"));
+            dataModels.add(new DataModel("Ice Cream Sandwich", "Android 4.0", "14","October 18, 2011"));
+            dataModels.add(new DataModel("Jelly Bean", "Android 4.2", "16","July 9, 2012"));
+            dataModels.add(new DataModel("Kitkat", "Android 4.4", "19","October 31, 2013"));
+            dataModels.add(new DataModel("Lollipop","Android 5.0","21","November 12, 2014"));
+            dataModels.add(new DataModel("Marshmallow", "Android 6.0", "23","October 5, 2015"));
+
+
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dataModels= new ArrayList<>();
+            dataModels.clear();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            adapter= new CustomAdapter(dataModels,getApplicationContext());
+            listView.setAdapter(adapter);
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                    DataModel dataModel = dataModels.get(position);
+
+                    Snackbar.make(view, dataModel.getName()+"\n"+dataModel.getType()+" API: "+dataModel.getVersion_number(), Snackbar.LENGTH_LONG)
+                            .setAction("No action", null).show();
+                }
+            });
+        }
+
+
+
     }
 }
